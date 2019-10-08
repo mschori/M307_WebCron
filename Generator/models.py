e8f0fedcf780955bbe001db3be882f15c830a90c
Michael Schori <michael.schori@gmx.ch>
2019-10-08 09:21:29 +0200

    First

--- Generator/models.py
+++ Generator/models.py
@@ -1,16 +1,14 @@
-from django.contrib.auth.models import User
 from django.db import models
 
 
 class CronJob(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=255, null=False, defaults='')
+    title = models.CharField(max_length=255, null=False, default='')
     url = models.URLField(max_length=255, null=False, default='')
     auth_enabled = models.BooleanField(default=False)
-    auth_user = models.CharField(max_length=255, null=True, defaults='')
-    auth_pw = models.CharField(max_length=255, null=True, default='')
+    auth_user = models.CharField(max_length=255, null=True)
+    auth_pw = models.CharField(max_length=255, null=True)
     exec_minute = models.CharField(max_length=255, null=False, default='')
-    exec_hour = models.IntegerField(max_length=255, null=False, default='')
+    exec_hour = models.CharField(max_length=255, null=False, default='')
     exec_day = models.CharField(max_length=255, null=False, default='')
     exec_month = models.CharField(max_length=255, null=False, default='')
     exec_weekday = models.CharField(max_length=255, null=False, default='')
